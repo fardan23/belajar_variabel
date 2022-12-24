88b8c9a904057fad211a406fc4869b8972b9e8cb
Muhamad Fardan <mprdan23@gmail.com>
2022-12-24 12:42:29 +0700

    menambah semua file

---
+++ src/muhamad_fardan/Muhamad_fardan.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muhamad_fardan;
+
+/**
+ *
+ * @author FARDAN
+ */
+public class Muhamad_fardan {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*System.out.println("NIM):G.131.10.0049");
+        System.out.println("Nama):Muhamad Fardan");
+        System.out.println("Alamat):Kp.Cibeureum");
+        System.out.println("Kota):Bogor");
+        System.out.println("Kode POS):16820");
+        System.out.println("TELEPON):081298937681");
+        System.out.println("Handphone):081298937681");
+        System.out.println("EMAIL):fardanadan97@gmail.com");*/
+        
+        /*int p,l,luas;
+        p=7;
+        l=12;
+        luas=p*l;
+        System.out.println("Menghitung Luas Persegi Panjang");
+        System.out.println("Panjang ="+p);
+        System.out.println("Lebar ="+l);
+        System.out.println("Luas ="+luas);
+        
+        System.out.println("ini merupakan percobaan pertama ku");
+        System.out.println("Hari Rabu,07 September 2022");*/
+        
+        /*int r;
+        float phi=3.14f,l;
+        r=20;
+        l=phi*r*r;
+        System.out.println("Jari-jari = "+r);
+        System.out.println("Phi = "+phi);
+        System.out.println("Luas = "+l);
+        // TODO code application logic here*/
+        
+        /*int a,t;
+        float l;
+        a=20;
+        t=10;
+        l=0.5f*a*t;
+        
+        System.out.println("Alas = "+a);
+        System.out.println("Tinggi = "+t);
+        System.out.println("Luas = "+l);*/
+        
+        /*class mobil{
+            String warna, hidupkan, ubah, matikan;
+            int tahunproduksi;
+        }
+        mobil mobilku = new mobil();
+        mobil mobilmu = new mobil();
+        
+        //Memanggil atribut dan memberi nilai
+        mobilku.warna = "Hitam";
+        mobilku.tahunproduksi = 2006;
+        mobilku.hidupkan = "Hitam";
+        mobilku.ubah = "Hitam";
+        mobilku.matikan = "Hitam";
+        mobilmu.warna = "Hijau";
+        mobilmu.tahunproduksi = 2010;
+        
+        System.out.println("Warna:"+mobilku.warna);
+        System.out.println("Tahun:"+mobilku.tahunproduksi);
+        System.out.println("Warna:"+mobilmu.warna);
+        System.out.println("Tahun:"+mobilmu.tahunproduksi);
+        System.out.println("Hidupkan mobil:"+mobilku.hidupkan);
+        System.out.println("Ubah mobil:"+mobilku.ubah);
+        System.out.println("Matikan mobil:"+mobilku.matikan);*/
+        
+        /*class Nilai{
+            String nim, nama;
+            float nilaiuts,nilaitugas,nilaiuas,pnilaiuts,pnilaitugas,pnilaiuas,nilaiakhir;
+            void hitungNilai()
+            {
+                pnilaiuts=0.2f*nilaiuts;
+                pnilaitugas=0.35f*nilaitugas;
+                pnilaiuas=0.45f*nilaiuas;
+                nilaiakhir=pnilaiuts+pnilaitugas+pnilaiuas;
+            }
+            void cetakNilai()
+            {
+                System.out.println("NIM ="+nim);
+                System.out.println("Nama ="+nama);
+                System.out.println("Nilai UTS ="+nilaiuts+" 20% :"+pnilaiuts);
+                System.out.println("Nilai Tugas ="+nilaitugas+" 35% :"+pnilaitugas);
+                System.out.println("Nilai UAS ="+nilaiuas+" 45% :"+pnilaiuas);
+                System.out.println("Nilai akhir ="+nilaiakhir);
+            }
+        }
+        
+                Nilai Nilaiku=new Nilai();
+                
+                Nilaiku.nim="G.131.10.0049";
+                
+                Nilaiku.nama="Fardan";
+                Nilaiku.nilaitugas=80;
+                Nilaiku.nilaiuts=87;
+                Nilaiku.nilaiuts=90;
+                
+                Nilaiku.hitungNilai();
+                Nilaiku.cetakNilai();*/
+            }
+        }
